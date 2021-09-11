a761ebd751f11cd6370edf889e9f30755a3c7691
Summers Pittman <supittma@redhat.com>
2021-09-11 11:19:28 -0400

    stuff

--- chuubos-web/src/app/Dashboard/Dashboard.tsx
+++ chuubos-web/src/app/Dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 import { useState } from 'react';
+import { Config } from '../utils/Config'
+
 
 const Dashboard: React.FunctionComponent = () => {
 
+  const apiUrl = Config.getAPIServerURL() || 'http://localhost:8080';
+
   const [connected, setConnected] = useState(false);
 
-  fetch("http://localhost:8080/hello").then(value => {value.status == 200 ? setConnected(true): console.log("Error!")});
+  fetch( apiUrl + "/hello").then(value => {value.status == 200 ? setConnected(true): console.log("Error!")});
 
  return ( <PageSection>
     <Title headingLevel="h1" size="lg">{connected?"Connected!":"Not Connected!"}</Title>
